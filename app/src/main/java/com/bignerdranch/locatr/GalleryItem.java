20d2ff25feadbaffbc793d4fcaaf0eda20b4a871
E-Mark21 <yevheniimarkosh@gmail.com>
2020-09-30 16:22:00 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/locatr/GalleryItem.java
@@ -0,0 +1,73 @@
+package com.bignerdranch.locatr;
+
+import android.net.Uri;
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    private String mOwner;
+    private Double mLat;
+    private Double mLon;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Double getLat() {
+        return mLat;
+    }
+
+    public void setLat(Double lat) {
+        mLat = lat;
+    }
+
+    public Double getLon() {
+        return mLon;
+    }
+
+    public void setLon(Double lon) {
+        mLon = lon;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+}
